ced62f671a550cf7e6d4e67fee86ae25d90a044d
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-05-23 20:52:07 -0400

    commit tree c7a7b37d8cd6e0740a1545f5a28fc2734e0c31a9

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+ 
+
   render() {
+    const charLeft = this.props.maxChars - this.state.message.length
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <strong><span>{charLeft}</span>Your message:</strong>
+        <input onChange={this.handleChange} type="text" name="message" value={this.state.message} id="message" />
       </div>
     );
   }
